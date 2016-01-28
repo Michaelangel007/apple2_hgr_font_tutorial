bb203b7b86d9c7fe9147d3d0a5d6a68ea7f5fca4
Michaelangel007 <testcraft@sharedcraft.com>
2016-01-27 20:49:54 -0800

    Added script to build hgrfont.dsk

---
+++ bin/build_font_dsk.sh
@@ -0,0 +1,14 @@
+!#/bin/sh
+COPY=cp
+
+ source src2dsk.sh ../asm/drawfont.s
+ source src2dsk.sh ../asm/charinspect.s
+ source src2dsk.sh ../asm/font7x8.s
+ source src2dsk.sh ../asm/fontbb.s
+
+${COPY} emptydos33.dsk hgrfont.dsk
+ a2in -r b hgrfont.dsk DRAWFONT    drawfont.bin
+ a2in -r b hgrfont.dsk CHARINSPECT charinspect.bin
+ a2in -r b hgrfont.dsk FONT7X8     font7x8.bin
+ a2in -r b hgrfont.dsk FONTBB      fontbb.bin
+
