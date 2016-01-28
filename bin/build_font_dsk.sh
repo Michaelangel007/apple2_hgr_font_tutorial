3086ed363970a948dec135dc6ff7490087e8b614
Michaelangel007 <testcraft@sharedcraft.com>
2016-01-27 23:27:27 -0800

    Update build dsk script to add asiitable

--- bin/build_font_dsk.sh
+++ bin/build_font_dsk.sh
@@ -5,10 +5,12 @@ COPY=cp
  source src2dsk.sh ../asm/charinspect.s
  source src2dsk.sh ../asm/font7x8.s
  source src2dsk.sh ../asm/fontbb.s
+ source src2dsk.sh ../asm/asciitable.s
 
 ${COPY} emptydos33.dsk hgrfont.dsk
  a2in -r b hgrfont.dsk DRAWFONT    drawfont.bin
  a2in -r b hgrfont.dsk CHARINSPECT charinspect.bin
  a2in -r b hgrfont.dsk FONT7X8     font7x8.bin
  a2in -r b hgrfont.dsk FONTBB      fontbb.bin
+ a2in -r b hgrfont.dsk ASCIITABLE  asciitable.bin
 
